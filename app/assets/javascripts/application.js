60e55a9c8cc7ec986b2df7d1a3dba990d158b83f
jjreich <jj.reich@nalusolutions.com>
2020-09-06 14:11:48 -0500

    slider working to set arrival time of client.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
 //= require_tree .
 
-$(document).on('turbolinks:load', function() {
-    $(function(){ $(document).foundation(); });
-});
+$(function(){ $(document).foundation(); });
