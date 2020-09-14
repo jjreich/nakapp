b09cfa43b767217557bb4c8fa801f36d4449c8bf
jjreich <jj.reich@nalusolutions.com>
2020-09-14 17:48:52 -0500

    added firstandlastnames to user login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   private
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(users)
     root_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+	devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName])
+	devise_parameter_sanitizer.permit(:sign_in, keys: [:firstName, :lastName])
+	devise_parameter_sanitizer.permit(:account_update, keys: [:firstName, :lastName])
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :fbos
   resources :aircrafts
   resources :airfields
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
