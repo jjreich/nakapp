bd703fd5090130d9f4354272d442721e9d878157
jjreich <jj.reich@nalusolutions.com>
2020-09-15 15:26:17 -0500

    added overall star value to debrier page.

--- app/models/debrief.rb
+++ app/models/debrief.rb
@@ -3,4 +3,45 @@ class Debrief < ApplicationRecord
 	belongs_to :flight
 	has_many :comments
 
+	def overall_rating
+
+	  ratingsum = 0
+	  ratingcount = 0
+
+	  if preparationRating.present?
+	  	ratingsum = ratingsum + preparationRating
+	  	ratingcount = ratingcount + 1
+	  end 
+
+	  if cateringRating.present?
+	  	ratingsum = ratingsum + cateringRating
+	  	ratingcount = ratingcount + 1
+	  end
+
+	  if client_arrival_rating.present?
+	  	ratingsum = ratingsum + client_arrival_rating
+	  	ratingcount = ratingcount + 1
+	  end
+
+	  if airborne_rating.present?
+	  	ratingsum = ratingsum + airborne_rating
+	  	ratingcount = ratingcount + 1
+	  end
+
+	  if client_departure_rating.present?
+	  	ratingsum = ratingsum + client_departure_rating
+	  	ratingcount = ratingcount + 1
+	  end
+
+	  ratingsum = ratingsum.to_f
+	  overall_rating = (ratingsum / ratingcount)
+
+	end
+
+	def star_rating_value
+
+		star_rating_value = overall_rating.divmod 1
+
+	end			
+
 end
