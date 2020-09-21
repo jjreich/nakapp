9b76e3da60345114e5f434ab592d1495cdbf1b60
jjreich <jj.reich@nalusolutions.com>
2020-09-21 18:39:12 -0500

    working to get the devise emails working.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'app183288585@heroku.com',
+    :password       => 'kznhfxbv0522',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true 
   }
