21449f985a4783260424b533882aff1ed20a4ec2
jjreich <jj.reich@nalusolutions.com>
2020-09-27 13:37:51 -0500

    fixed problem with comment not closing when submitting.

--- app/controllers/debriefs_controller.rb
+++ app/controllers/debriefs_controller.rb
@@ -1,6 +1,5 @@
 class DebriefsController < ApplicationController
   before_action :set_debrief, only: [:show, :edit, :update, :destroy]
-  before_action :set_debriefs, only: [:index, :show, :create, :update]
 
   respond_to :html, :js
 
@@ -96,10 +95,6 @@ class DebriefsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_debrief
       @debrief = Debrief.find(params[:id])
-    end
-
-    def set_debriefs
-      @debriefs = Debrief.where(finish_later: nil).includes(:flight).order("flights.created_at desc").page params[:page]
     end
 
     # Only allow a list of trusted parameters through.
