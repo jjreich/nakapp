da1c92532d59c9ece1a735d17971f33dcc17b28c
jjreich <jj.reich@nalusolutions.com>
2020-09-26 19:35:17 -0500

    added users controller and ability to manage userss from admin login.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :aircrafts
   resources :airfields
   devise_for :users, controllers: { sessions: 'users/sessions'}
+  resources :users, :only => [:show, :edit, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
